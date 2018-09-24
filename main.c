9f14a9bf0f2dcb4f07283c1b1febc2581bf0ffe8
Alan Zhao <alan.zhao@despegar.com>
2018-09-24 02:00:10 -0300

    Continuous input request logic implemented.

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define SI 'S'
+
+bool askForAnotherLexicalCheck();
 
 int main() {
-    printf("Hello, World!\n");
+    bool lexicalCheckRequired;
+
+    do {
+        lexicalCheckRequired = askForAnotherLexicalCheck();
+    } while (lexicalCheckRequired);
+
     return 0;
+}
+
+bool askForAnotherLexicalCheck() {
+    char answer = 'N';
+    printf("¿Desea ingresar otra cadena? S/N: ");
+    scanf(" %c", &answer);
+    fflush(stdout);
+    return toupper(answer) == SI ? true : false;
 }
