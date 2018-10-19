18d8bca9086f4b0ffa32ac06448d7f3537f9f495
Alan Zhao <alan.zhao@despegar.com>
2018-10-19 04:06:45 -0300

    Automata base feature implemented.

--- main.c
+++ main.c
@@ -5,23 +5,49 @@
 #include <string.h>
 
 #define YES 'S'
-#define FDT EOF
+
+struct _Buffer;
+struct _Automata;
 
 bool askForAnotherLexicalCheck();
 
+bool notFDT(struct _Buffer buffer);
+
 char *requestStringInputToCheck();
 
-struct _Buffer;
+typedef enum {
+    INITIAL, FINAL, NONE
+} STATE_PROPERTY;
+
+typedef struct _State {
+    int id;
+    STATE_PROPERTY stateProperty;
+} State;
+
+typedef void (*SetActualStateToInitialState)(struct _Automata *self);
+
+typedef struct _Automata {
+    State actualState;
+    SetActualStateToInitialState setActualStateToInitialState;
+} Automata;
+
+void Automata__setActualStateToInitialState(Automata *self) {
+    //TODO: implementar la tabla. Necesito poder decirle que solito vaya al estado inicial (de verdad / no hardcoded).
+    State initialState = {.id = 0, .stateProperty = INITIAL};
+    self->actualState = initialState;
+}
 
 typedef char (*FetchNextCharacter)(struct _Buffer *self);
 
-typedef struct _Automata {
-    int id;
-} Automata;
+typedef char (*IsFDT)(struct _Buffer *self);
+
+typedef void (*Clean)(struct _Buffer *self);
 
 typedef struct _Buffer {
     char *input;
     FetchNextCharacter fetchNextCharacter;
+    IsFDT isFDT;
+    Clean clean;
 } Buffer;
 
 char Buffer__fetchNextCharacter(Buffer *self) {
@@ -35,48 +61,89 @@ char Buffer__fetchNextCharacter(Buffer *self) {
     return nextCharacter;
 }
 
+char Buffer__isFDT(Buffer *self) {
+    char *c = self->input;
+    return ((c != NULL) && (c[0] == '\0'));
+}
+
+void Buffer__clean(Buffer *self) {
+    free(self->input);
+}
+
 /*
  * Algoritmo 3
-- Intenta leer primer carácter del texto (porque el texto puede estar
-vacío)
-- Mientras no sea fdt, repetir:
-(1) Estado actual del autómata: estado inicial
-(2) Mientras no sea un estado final y no sea el estado FDT, repetir:
-(2.1) Determinar el nuevo estado actual
-(2.2) Actualizar el carácter a analizar
-(3) Si el estado es final, la cadena procesada es una constante entera;
-caso contrario, la cadena no pertenece al lenguaje.
+ *
+ * - Intenta leer primer carácter del texto (porque el texto puede estar vacío)
+ * - Mientras no sea fdt, repetir:
+ * (1) Estado actual del autómata: estado inicial
+ * (2) Mientras no sea un estado final y no sea el estado FDT, repetir:
+ * (2.1) Determinar el nuevo estado actual
+ * (2.2) Actualizar el carácter a analizar
+ * (3) Si el estado es final, la cadena procesada es una constante entera;
+ * caso contrario, la cadena no pertenece al lenguaje.
+ *
+ * ER: [01]\.[0-9]{2}|[01]*B Centinela: %
+ * //TODO: Preguntar si pueden ingresar caracters que no forman parte del alfabeto. ¿A que estado voy?
+ * +-----+-----------+-----+-----+---+
+ * | AFD | . (punto) | 0-1 | 2-9 | B |
+ * +-----+-----------+-----+-----+---+
+ * | 0-  |           |  1  |     | 5 |
+ * | 1   |     2     |  4  |     | 5 |
+ * | 2   |           |  3  |  3  |   |
+ * | 3   |           |  5  |  5  |   |
+ * | 4   |           |  4  |     | 5 |
+ * | 5+  |           |     |     |   |
+ * +-----+-----------+-----+-----+---+
+ *
  */
 
 int main() {
     bool lexicalCheckRequired;
-    Buffer buffer = {.fetchNextCharacter = Buffer__fetchNextCharacter};
+    Buffer buffer = {.fetchNextCharacter = Buffer__fetchNextCharacter,
+            .isFDT = Buffer__isFDT,
+            .clean = Buffer__clean};
+    Automata automata = {.setActualStateToInitialState = Automata__setActualStateToInitialState};
 
     do {
-        buffer.input = requestStringInputToCheck();
 
+        buffer.input = requestStringInputToCheck();
         char textCharacter = buffer.fetchNextCharacter(&buffer);
-        /*while (!IsFDT(textCharacter)) { //- Mientras no sea fdt, repetir:
-            automata.setActualStateToInitialState(); // (1) Estado actual del autómata: estado inicial
-            while (!automata.stateIsFinalOrFDT()) { // (2) Mientras no sea un estado final y no sea el estado FDT, repetir:
+        //TODO:diferenciar FDT del centinela.
+        //FDT para el buffer es un concepto. Responde indicando si le quedan caracteres por entregar.
+        //El centinela, si lo encuentro, marca un corte.
+        while (notFDT(buffer)) { //- Mientras no sea fdt, repetir:
+            automata.setActualStateToInitialState(&automata); // (1) Estado actual del autómata: estado inicial
+            /*while (!automata.stateIsFinalOrFDT()) { // (2) Mientras no sea un estado final y no sea el estado FDT, repetir:
                 automata.determineCurrentState(textCharacter); // (2.1) Determinar el nuevo estado actual
-                buffer.add(textCharacter);
-                textCharacter = fetchNextCharacterFromInput(); // (2.2) Actualizar el carácter a analizar
+                prettyPrinter.append(textCharacter);
+                textCharacter = buffer.fetchNextCharacter(&buffer); // (2.2) Actualizar el carácter a analizar
+                if (textCharacter == SENTINEL) {
+                    break;
+                }
             }
             if (automata.actualStateIsFinal()) { // (3) Si el estado es final, la cadena procesada es una constante entera;
-                buffer.print(); // o hacer un buffer.getAll() con un %s.
+                //Deprecado en favor de pretty print <- buffer.print(); // o hacer un buffer.getAll() con un %s.
                 printf("¡La cadena pertenece al lenguaje!\n");
-                buffer.clean();
+                prettyPrinter.persistString(palabra);
+                //PP <- buffer.clean();
             } else { // caso contrario, la cadena no pertenece al lenguaje.
                 printf("La cadena no pertenece al lenguaje.\n");
-                buffer.clean();
-            }
-        }*/
-        free(buffer.input);
+                prettyPrinter.flush();
+                //PP <- buffer.clean();
+            }*/
+            printf("LOL\n");
+            break;
+        }
+        buffer.clean(&buffer);
+        //prettyPrinter.printResults()
         lexicalCheckRequired = askForAnotherLexicalCheck();
     } while (lexicalCheckRequired);
 
     return 0;
+}
+
+bool notFDT(Buffer buffer) {
+    return !buffer.isFDT(&buffer);
 }
 
 bool askForAnotherLexicalCheck() {
